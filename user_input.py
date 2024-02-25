46f50c80cf6ccfdc4abe759c65b8986e8461bf5e
Arthur Mulopa <61255839+CaptainFalc0n@users.noreply.github.com>
2024-02-25 11:06:54 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,11 @@
+#ask for the name
+name = input("Enter your name: ")
+
+#ask for age
+age = input("Enter your age: ")
+
+#ask for location
+location = input("Enter your location: ")
+
+#personalized text
+print(f"Hello {name}, you are {age} years old and you live in {location}")
